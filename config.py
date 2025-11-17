0795bcabf4443c521d453ae88493017497f6af9c
herobotk <herobot642@gmail.com>
2025-11-17 12:23:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,20 @@
-API_ID = int(__import__('os').getenv('API_ID'))
-API_HASH = __import__('os').getenv('API_HASH')
+import os
 
-BOT_TOKEN_1 = __import__('os').getenv("BOT_TOKEN_1")  # @BOSS_replybot
-BOT_TOKEN_2 = __import__('os').getenv("BOT_TOKEN_2")  # @Hosper_x_robot
+# MAIN AUTH
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+# BOT TOKENS
+BOT_TOKEN_1 = os.getenv("BOT_TOKEN_1")   # BOT 1 → KillMe + Filter
+BOT_TOKEN_2 = os.getenv("BOT_TOKEN_2")   # BOT 2 → Reply Bot Only
+
+
+# FOR BOT 1 (Kill Me Bot Channels)
+def get_list(env):
+    return [int(x.strip()) for x in os.getenv(env, "").split(",") if x.strip()]
+
+KILLME_CHANNELS = get_list("KILLME_CHANNELS")
+
+# FOR BOT 2 (Reply Bot Groups)
+REPLYBOT_GROUP = get_list("REPLYBOT_GROUP")
+GROUP_EXCLUDED_IDS = get_list("GROUP_EXCLUDED_IDS")
